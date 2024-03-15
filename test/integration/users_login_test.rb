50135db15b96e53f8b21d71d4ce99f462f6d5ac9
siTechouse <163281686+siTechouse@users.noreply.github.com>
2024-03-15 04:57:15 +0000

    Tutorial 11.3 end

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -5,7 +5,6 @@ class UsersLogin < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
   end
-
 end
 
 class InvalidPasswordTest < UsersLogin
@@ -78,14 +77,13 @@ class LogoutTest < Logout
     delete logout_path
     assert_redirected_to root_url
   end
-  
 end
 
 class RememberingTest < UsersLogin
 
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
-    assert_equal cookies[:remember_token], assigns(:user).remember_token
+    assert_not cookies[:remember_token].blank?
   end
 
   test "login without remembering" do
@@ -95,5 +93,4 @@ class RememberingTest < UsersLogin
     log_in_as(@user, remember_me: '0')
     assert cookies[:remember_token].blank?
   end
-
 end
